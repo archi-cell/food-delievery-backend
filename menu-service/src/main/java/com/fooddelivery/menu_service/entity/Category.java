bf03c271d8e35a9ed02c278e64a1c55cf239acf1
archi-cell <159535338+archi-cell@users.noreply.github.com>
2026-01-22 22:31:46 +0530

    Create Category.java

---
+++ menu-service/src/main/java/com/fooddelivery/menu_service/entity/Category.java
@@ -0,0 +1,18 @@
+package com.fooddelivery.menu_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
